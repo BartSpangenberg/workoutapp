80921f7b4121b73573f075e3a3160b0e69e99c86
Anja Ramihone <AnjaRL@users.noreply.github.com>
2021-07-02 23:02:01 +0400

    auth hbs & routes not totally done

--- models/User.model.js
+++ models/User.model.js
@@ -2,11 +2,48 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
   username: {
     type: String,
-    // unique: true -> Ideally, should be unique, but its up to you
+    required: true,
+  },
+  trainername: {
+    type: String,
+    required: true,
   },
   password: String,
+  athleteType: {
+    type: String,
+    required: true,
+    enum: ["Lannister / Targaryan", "Beginner", "Intermediate", "Pro", "Stark"],
+  },
+  height: {
+    type: Number,
+    required: true,
+  },
+  weight: {
+    type: Number,
+    required: true,
+  },
+  birthday: {
+    type: Date,
+    required: true,
+  },
+  goals: {
+    type: String,
+    required: true,
+    enum: [
+      "Get Summer fit",
+      "More Athletic",
+      "Lose Weight",
+      "Run a Marathon",
+      "Improve endurance",
+      "Build muscle",
+    ],
+  },
 });
 
 const User = model("User", userSchema);
