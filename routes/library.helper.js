0355324cecfd361d6ce4313bf7a0b7684ca9ca69
Bart Spangenberg <bart_spangenberg@protonmail.com>
2021-07-04 14:25:31 +0200

    Refactor code structure by seperating helper functions from router functions

---
+++ routes/library.helper.js
@@ -0,0 +1,209 @@
+const { types, levels, goalsArr, intensities, unitTypes, equipments, muscles } = require('../data/workoutData');
+
+function saveWorkoutDataInTheSession(req) { 
+    const {name, description, duration, type, level, goals, intensity, reps, unitType, restBetweenExercises, sets, restBetweenSets, exerciseName, exerciseId} = req.body;
+    let updatedWorkout = {
+        name,
+        description,
+        duration,
+        type: {
+            Selected: type,
+            NotSelected: createArrayOfNotSelectedItems(types, type)
+        },
+        level: {
+            Selected: level,
+            NotSelected: createArrayOfNotSelectedItems(levels, level)
+        },
+        goals: {
+            Selected: goals,
+            NotSelected: createArrayOfNotSelectedItems(goalsArr, goals)
+        },
+        intensity: {
+            Selected: intensity,
+            NotSelected: createArrayOfNotSelectedItems(intensities, intensity)
+        },
+        exercises: !reps ? [] : turnExerciseArraysIntoOneArrayOfExerciseObjects(reps, unitType, restBetweenExercises, sets, restBetweenSets, exerciseName, exerciseId),
+        exerciseIds: req.session.workout.exerciseIds,
+        exerciseNames: req.session.workout.exerciseNames
+    }	
+    
+    req.session.workout = updatedWorkout
+}
+
+function turnExerciseArraysIntoOneArrayOfExerciseObjects(reps, unitType, restBetweenExercises, sets, restBetweenSets, exerciseName, exerciseId) {
+    let exercises = [];
+    if (typeof reps === 'string') {
+        let newExercise = {};
+        newExercise = {
+            reps, 
+            sets,
+            restBetweenSets,
+            restBetweenExercises,
+            unitTypes: {
+                Selected: unitType,
+                NotSelected: createArrayOfNotSelectedItems(unitTypes, unitType)
+            }
+        }
+        exercises.push(newExercise);
+    }
+    else {
+        for (let i = 0; i < reps.length; i++) {
+            let newExercise = {
+                reps: reps[i],
+                unitType: unitType[i],
+                sets: sets[i],
+                restBetweenSets: restBetweenSets[i],
+                restBetweenExercises: restBetweenExercises[i],
+                unitTypes: {
+                    Selected: unitType[i],
+                    NotSelected: createArrayOfNotSelectedItems(unitTypes, unitType[i])
+                }
+            }
+            exercises.push(newExercise);
+        }   
+    }
+    return exercises;
+}
+
+function turnSessionDataIntoWorkoutObject(req) {
+    let { name, description, duration, type, level, goals, intensity, exercises, exerciseNames } = req.session.workout
+    exercises = addExerciseNamesToWorkoutObj(exercises, exerciseNames);
+    const workoutObj = { name, description, duration, type, level, goals, intensity, exercises };
+    return workoutObj
+}
+
+function createArrayOfNotSelectedItems(array, selectedItem) {
+    const cloneArr = JSON.parse(JSON.stringify(array))
+    let indexSelectedItem = cloneArr.indexOf(selectedItem);
+    cloneArr.splice(indexSelectedItem, 1);
+    return cloneArr;
+}
+
+function resetSessionWorkoutData(req, res, next) {
+    if (!req.session.isCreatingWorkout) {
+        req.session.workout = {
+            name: '',
+            description: '',
+            duration: 10,
+            type: {
+                Selected: types[0],
+                NotSelected: createArrayOfNotSelectedItems(types, types[0])
+            },
+            level: {
+                Selected: levels[0],
+                NotSelected: createArrayOfNotSelectedItems(levels, levels[0])
+            },
+            goals: {
+                Selected: goalsArr[0],
+                NotSelected: createArrayOfNotSelectedItems(goalsArr, goalsArr[0])
+            },
+            intensity: {
+                Selected: intensities[0],
+                NotSelected: createArrayOfNotSelectedItems(intensities, intensities[0])
+            },
+            exercises: [],
+            exerciseIds: [],
+            exerciseNames: []
+        }
+    }
+    next();
+}
+
+function createIsCreatingWorkout(req, res, next) {
+    // Temporary variable which needs to be created on cookie when the user logs in
+    if (!('isCreatingWorkout' in req.session)) {
+        req.session.isCreatingWorkout = false;
+    }
+    next();
+}
+
+function convertCheckboxData(object, array) {
+    let newArr = [];
+    for (key in object) {
+        array.forEach(el => {
+            if(key === el) {
+                newArr.push(key)
+            }
+        })
+    }
+    return newArr;
+}
+
+function extractWorkoutDataFromSession(object) {
+    return object.Selected
+}  
+
+function addIdsToExercises(exercises, ids) {
+    exercises.forEach((exercise, index) => {
+        exercise.exerciseId = ids[index];
+        exercise.unitType = exercise.unitTypes.Selected;
+    })
+    return exercises
+}
+
+function addExerciseNamesToWorkoutObj(exercises, exerciseNames) {
+    for (let i = 0; i < exercises.length; i++) {
+        exercises[i].exerciseName = exerciseNames[i];
+    }
+    return exercises
+}
+
+function createWorkoutObject(workoutObjectFromSession) {
+    const {name, description, duration, type, level, goals, intensity, exercises, exerciseIds} = workoutObjectFromSession;
+    let newWorkout = {
+        name,
+        description,
+        duration: +duration,
+        type: extractWorkoutDataFromSession(type),
+        athleteLevel: extractWorkoutDataFromSession(level),
+        goals: [extractWorkoutDataFromSession(goals)],
+        intensity: extractWorkoutDataFromSession(intensity),
+        exercises: addIdsToExercises(exercises, exerciseIds)
+    }
+    return newWorkout
+}
+
+function createNewExerciseObject(bodyObject, muscles, equipments) {
+    const {name , description} = bodyObject;
+
+    let muscleData = convertCheckboxData(bodyObject, muscles);
+    let equipmentData = convertCheckboxData(bodyObject, equipments);
+
+    let exerciseObj = {
+        name,
+        description,
+        equipments: equipmentData.length > 0 ? equipmentData : equipments[0],
+        muscles: muscleData.length > 0 ? muscleData : muscles[0]
+    } 
+    return exerciseObj
+}
+
+function createNewExerciseToAddToSession() {
+    let newExercise = {
+        reps: 10,
+        sets: 1,
+        restBetweenSets: 60,
+        restBetweenExercises: 30,
+        unitTypes: {
+            Selected: unitTypes[0],
+            NotSelected: unitTypes.slice(1)
+        }
+    }
+    return newExercise
+}
+
+module.exports = {
+    saveWorkoutDataInTheSession,
+    turnExerciseArraysIntoOneArrayOfExerciseObjects,
+    turnSessionDataIntoWorkoutObject,
+    createArrayOfNotSelectedItems,
+    resetSessionWorkoutData, 
+    createIsCreatingWorkout, 
+    convertCheckboxData, 
+    extractWorkoutDataFromSession, 
+    addIdsToExercises, 
+    addExerciseNamesToWorkoutObj, 
+    createWorkoutObject,
+    createNewExerciseObject,
+    createNewExerciseToAddToSession
+}
--- routes/library.routes.js
+++ routes/library.routes.js
@@ -1,11 +1,20 @@
 const router = require("express").Router();
 const ExerciseModel = require('../models/Exercise.model');
 const WorkoutModel = require('../models/Workout.model');
-const { types, levels, goalsArr, intensities, unitTypes, equipments, muscles } = require('../data/workoutData');
+const { equipments, muscles } = require('../data/workoutData');
+const {
+    saveWorkoutDataInTheSession,
+    turnSessionDataIntoWorkoutObject,
+    resetSessionWorkoutData, 
+    createIsCreatingWorkout, 
+    createWorkoutObject,
+    createNewExerciseObject,
+    createNewExerciseToAddToSession
+} = require('./library.helper')
 
 // createIsCreatingWorkout will later be removed when we create this variable on login
 router.get("/library/create-workout", createIsCreatingWorkout, resetSessionWorkoutData, (req, res, next) => {
-    const { exerciseNames } = req.session.workout;
+    // const { exerciseNames } = req.session.workout;
 
     let workoutObj = turnSessionDataIntoWorkoutObject(req);
 
@@ -23,33 +32,22 @@ router.post('/library/create-workout', (req, res, next) => {
 
     // After that the post request is handled based on the type of button click
     if (req.body.button === 'create-workout') {
-        const {name, description, duration, type, level, goals, intensity, exercises, exerciseIds} = req.session.workout;
-        let newWorkout = {
-            name,
-            description,
-            duration: +duration,
-            type: extractWorkoutDataFromSession(type),
-            athleteLevel: extractWorkoutDataFromSession(level),
-            goals: [extractWorkoutDataFromSession(goals)],
-            intensity: extractWorkoutDataFromSession(intensity),
-            exercises: addIdsToExercises(exercises, exerciseIds)
-        }
+        let newWorkout = createWorkoutObject(req.session.workout);
+
         console.log(newWorkout)
 
         WorkoutModel.create(newWorkout)
-            .then((createdWorkout) => {
+            .then(() => {
                 req.session.isCreatingWorkout = false;
                 res.send('Redirect to My Workouts');
-
-
             }).catch((err) => {
                 console.log("Something went wrong while creating a new workout", err)
                 res.redirect('/library/create-workout')
             });
     }
     else if (req.body.button === 'search-for-exercise') {
-        const { findExercise: searchWord } = req.body
-        req.session.exerciseSearchWord = searchWord;
+        // Store the searchword, this will later be used to search for the exercises
+        req.session.exerciseSearchWord = req.body.findExercise;
         res.redirect('/library/create-workout/exercise-pop-up')
     }
     else {
@@ -62,32 +60,16 @@ router.get("/library/create-exercise", (req, res, next) => {
 })
 
 router.post('/library/create-exercise', (req, res, next) => {
-    const {name , description} = req.body;
-    let muscleData = convertCheckboxData(req.body, muscles);
-    let equipmentData = convertCheckboxData(req.body, equipments);
-
-    let newExerciseForDatabase = {
-        name,
-        description,
-        equipments: equipmentData.length > 0 ? equipmentData : equipments[0],
-        muscles: muscleData.length > 0 ? muscleData : muscles[0]
-    } 
+    
+    let newExerciseForDatabase = createNewExerciseObject(req.body, muscles, equipments)
 
     ExerciseModel.create(newExerciseForDatabase)
         .then((exerciseData) => {
+            // Add new created exercise to session
             req.session.workout.exerciseIds.push(exerciseData._id);
             req.session.workout.exerciseNames.push(exerciseData.name);
-            let newExercise = {
-                reps: 10,
-                sets: 1,
-                restBetweenSets: 60,
-                restBetweenExercises: 30,
-                unitTypes: {
-                    Selected: 'Reps',
-                    NotSelected: ['Minutes', 'Meter', 'Km']
-                }
-            }
-            req.session.workout.exercises.push(newExercise)
+            let newExercise = createNewExerciseToAddToSession();
+            req.session.workout.exercises.push(newExercise);
 
             res.redirect('/library/create-workout')
         }).catch((err) => {
@@ -114,199 +96,13 @@ router.post('/library/create-workout/exercise-pop-up', (req, res, next) => {
     const { exerciseId }  = req.body;
     ExerciseModel.findById(exerciseId)
         .then((singleExercise) => {
-
             req.session.workout.exerciseIds.push(singleExercise._id);
             req.session.workout.exerciseNames.push(singleExercise.name);
-
-            req.session.workout.exercises.push({
-                reps: 10,
-                sets: 1,
-                restBetweenSets: 60,
-                restBetweenExercises: 30,
-                unitTypes: {
-                    Selected: 'Reps',
-                    NotSelected: ['Minutes', 'Meter', 'Km']
-                }
-            })
+            req.session.workout.exercises.push(createNewExerciseToAddToSession());
             res.redirect('/library/create-workout')
         }).catch((err) => {
             console.log('Something went wrong while searching for a single exercise', err)
         });
 })
-
-function saveWorkoutDataInTheSession(req) { 
-    const {name, description, duration, type, level, goals, intensity, reps, unitType, restBetweenExercises, sets, restBetweenSets, exerciseName, exerciseId} = req.body;
-    let updatedWorkout = {
-        name,
-        description,
-        duration,
-        type: {
-            Selected: type,
-            NotSelected: createArrayOfNotSelectedItems(types, type)
-        },
-        level: {
-            Selected: level,
-            NotSelected: createArrayOfNotSelectedItems(levels, level)
-        },
-        goals: {
-            Selected: goals,
-            NotSelected: createArrayOfNotSelectedItems(goalsArr, goals)
-        },
-        intensity: {
-            Selected: intensity,
-            NotSelected: createArrayOfNotSelectedItems(intensities, intensity)
-        },
-        exercises: !reps ? [] : turnExerciseArraysIntoOneArrayOfExerciseObjects(reps, unitType, restBetweenExercises, sets, restBetweenSets, exerciseName, exerciseId),
-        exerciseIds: req.session.workout.exerciseIds,
-        exerciseNames: req.session.workout.exerciseNames
-    }	
-    
-    req.session.workout = updatedWorkout
-}
-
-function turnExerciseArraysIntoOneArrayOfExerciseObjects(reps, unitType, restBetweenExercises, sets, restBetweenSets, exerciseName, exerciseId) {
-    let exercises = [];
-    if (typeof reps === 'string') {
-        let newExercise = {};
-        newExercise = {
-            reps, 
-            sets,
-            restBetweenSets,
-            restBetweenExercises,
-            unitTypes: {
-                Selected: unitType,
-                NotSelected: createArrayOfNotSelectedItems(unitTypes, unitType)
-            }
-        }
-        exercises.push(newExercise);
-    }
-    else {
-        for (let i = 0; i < reps.length; i++) {
-            let newExercise = {
-                reps: reps[i],
-                unitType: unitType[i],
-                sets: sets[i],
-                restBetweenSets: restBetweenSets[i],
-                restBetweenExercises: restBetweenExercises[i],
-                unitTypes: {
-                    Selected: unitType[i],
-                    NotSelected: createArrayOfNotSelectedItems(unitTypes, unitType[i])
-                }
-            }
-            exercises.push(newExercise);
-        }   
-    }
-    return exercises;
-}
-
-function turnSessionDataIntoWorkoutObject(req) {
-    let { name, description, duration, type, level, goals, intensity, exercises, exerciseNames } = req.session.workout
-    exercises = addExerciseNamesToWorkoutObj(exercises, exerciseNames);
-    const workoutObj = { name, description, duration, type, level, goals, intensity, exercises };
-    return workoutObj
-}
-
-function createArrayOfNotSelectedItems(array, selectedItem) {
-    const cloneArr = JSON.parse(JSON.stringify(array))
-    let indexSelectedItem = cloneArr.indexOf(selectedItem);
-    cloneArr.splice(indexSelectedItem, 1);
-    return cloneArr;
-}
-
-function resetSessionWorkoutData(req, res, next) {
-    if (!req.session.isCreatingWorkout) {
-        req.session.workout = {
-            name: '',
-            description: '',
-            duration: 10,
-            type: {
-                Selected: types[0],
-                NotSelected: createArrayOfNotSelectedItems(types, types[0])
-            },
-            level: {
-                Selected: levels[0],
-                NotSelected: createArrayOfNotSelectedItems(levels, levels[0])
-            },
-            goals: {
-                Selected: goalsArr[0],
-                NotSelected: createArrayOfNotSelectedItems(goalsArr, goalsArr[0])
-            },
-            intensity: {
-                Selected: intensities[0],
-                NotSelected: createArrayOfNotSelectedItems(intensities, intensities[0])
-            },
-            exercises: [],
-            exerciseIds: [],
-            exerciseNames: []
-        }
-
-        // req.session.workout = {};
-
-
-        // req.session.workout.exerciseIds = [];
-        // req.session.workout.exerciseNames = [];
-        // req.session.workout.name = '';
-        // req.session.workout.description = '';
-        // req.session.workout.duration = 10;
-
-        // req.session.workout.type = {};
-        // req.session.workout.type.Selected = types[0];
-        // req.session.workout.type.NotSelected = createArrayOfNotSelectedItems(types, types[0]);
-
-        // req.session.workout.level = {};
-        // req.session.workout.level.Selected = levels[0];
-        // req.session.workout.level.NotSelected = createArrayOfNotSelectedItems(levels, levels[0]);
-
-        // req.session.workout.goals = {};
-        // req.session.workout.goals.Selected = goalsArr[0];
-        // req.session.workout.goals.NotSelected = createArrayOfNotSelectedItems(goalsArr, goalsArr[0]);
-
-        // req.session.workout.intensity = {};
-        // req.session.workout.intensity.Selected = intensities[0];
-        // req.session.workout.intensity.NotSelected = createArrayOfNotSelectedItems(intensities, intensities[0]);
-
-        // req.session.workout.exercises = [];
-    }
-    next();
-}
-
-function createIsCreatingWorkout(req, res, next) {
-    // Temporary variable which needs to be created on cookie when the user logs in
-    if (!('isCreatingWorkout' in req.session)) {
-        req.session.isCreatingWorkout = false;
-    }
-    next();
-}
-
-function convertCheckboxData(object, array) {
-    let newArr = [];
-    for (key in object) {
-        array.forEach(el => {
-            if(key === el) {
-                newArr.push(key)
-            }
-        })
-    }
-    return newArr;
-}
-
-function extractWorkoutDataFromSession(object) {
-    return object.Selected
-}  
-
-function addIdsToExercises(exercises, ids) {
-    exercises.forEach((exercise, index) => {
-        exercise.exerciseId = ids[index];
-        exercise.unitType = exercise.unitTypes.Selected;
-    })
-    return exercises
-}
-
-function addExerciseNamesToWorkoutObj(exercises, exerciseNames) {
-    for (let i = 0; i < exercises.length; i++) {
-        exercises[i].exerciseName = exerciseNames[i];
-    }
-    return exercises
-}
 
 module.exports = router;
