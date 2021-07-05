a5b951a6059544779b72f923c78cb149e9b23bae
Bart Spangenberg <bart_spangenberg@protonmail.com>
2021-07-05 16:38:06 +0200

    Pagination seems to work fine, small bug that I tried to fix magically disappeared, might still be there

--- routes/search-workout.routes.js
+++ routes/search-workout.routes.js
@@ -10,22 +10,45 @@ const {
 
 const searchOptions = createOptionsForAdvancedSearchForm();
 let searchData = turnSearchRequestIntoQueryData();
+let basicSearchData = turnSearchRequestIntoQueryData();
 const workoutsPerPage = 3;
 let workoutCount = 0;
 let pageCount = 1;
 let skipCount = 0;
+let basicSkipCount = 0;
 let pageNumberArr;
 
 router.get('/library/search', (req, res, next) => {
-    if (!("workoutName" in req.query)) {
-        res.render('library/search-basic.hbs');
-        return;
-    } 
+    const { workoutName, button, paginationBtn } = req.query;
+    // When the user clicks on the search button a new search query needs to be created
+    if (button === 'search') {
+        basicSearchData = turnSearchRequestIntoQueryData(workoutName);
+        // The search data needs to be stored in the session so that it can be re-called when the user clicks on a pagination button
+        req.session.basicSearchData = basicSearchData;
+    }
 
-    const workoutSearchWord = req.query.workoutName;
-    WorkoutModel.find({ "name": { "$regex": workoutSearchWord, "$options": "i" }}) 
+    if ("paginationBtn" in req.query) {
+        basicSkipCount =  workoutsPerPage * (+paginationBtn) - workoutsPerPage;
+    }
+
+    // The workoutCount of the query will be used to determine how many pages there will be
+    basicSearchData = req.session.basicSearchData;
+    WorkoutModel
+        .countDocuments({ "name": { "$regex": basicSearchData.workoutName, "$options": "i" }})
+            .then((count) => {
+                workoutCount =  count;
+                pageCount = Math.ceil( workoutCount / workoutsPerPage );
+                pageNumberArr = createPageNumberArr(pageCount);
+                basicSkipCount = 0;
+            }).catch((err) => {
+                console.log("something went wrong while counting the workouts" , err);
+            });
+
+    WorkoutModel.find({ "name": { "$regex": basicSearchData.workoutName, "$options": "i" }}) 
+        .skip(basicSkipCount)   
+        .limit(workoutsPerPage)
         .then((foundWorkouts) => {
-            res.render('library/search-basic.hbs', {foundWorkouts});
+            res.render('library/search-basic.hbs', {foundWorkouts, pageNumberArr });
         }).catch((err) => {
             console.log('Something went wrong while searching for workouts', err)  
         });
@@ -42,18 +65,18 @@ router.get('/library/search/advanced', (req, res, next) => {
     }
 
     if ("paginationBtn" in req.query) {
-        console.log(+paginationBtn)
         skipCount =  workoutsPerPage * (+paginationBtn) - workoutsPerPage;
-        console.log('Skipcount:', skipCount)
     }
 
     // The workoutCount of the query will be used to determine how many pages there will be
+    SearchData = req.session.SearchData;
     WorkoutModel
         .countDocuments({ $and: [{ "name": { "$regex": searchData.workoutName, "$options": "i" },  type: { $in: searchData.type }, athleteLevel: { $in: searchData.level }, intensity: { $in: searchData.intensity }, goals: { $in: searchData.goals }, duration: { $gte: searchData.minDuration, $lte: searchData.maxDuration } }] })
             .then((count) => {
                 workoutCount =  count;
                 pageCount = Math.ceil( workoutCount / workoutsPerPage );
                 pageNumberArr = createPageNumberArr(pageCount);
+                skipCount = 0;
             }).catch((err) => {
                 console.log("something went wrong while counting the workouts" , err);
             });
@@ -68,7 +91,6 @@ router.get('/library/search/advanced', (req, res, next) => {
         .limit(workoutsPerPage)
         .exec()
             .then((foundWorkouts) => {
-                // console.log(foundWorkouts)
                 res.render('library/search-advanced.hbs', { searchOptions, foundWorkouts, pageNumberArr });
             }).catch((err) => {
                 console.log('Something went wrong while searching for workouts', err)  
